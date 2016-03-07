1078d8cfcc39df2f1ed5884317a11e4f5e273c54
moss <moss@ip144-224.mphy.lu.se>
2016-03-07 15:28:39 +0100

    mja, får tänka om med has_and_belongs_to tror jag

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,3 @@
 class Actor < ActiveRecord::Base
+	has_and_belongs_to_many :requests, :class_name => "Request", :join_table => :actors_requests
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ActiveRecord::Base
 	has_one :business_context, :class_name => "BusinessContext"
+	has_and_belongs_to_many :actors,  :class_name => "Actor", :join_table => "actors_requests"
 	validates :name, presence: true, length: { minimum: 5 }
 end
