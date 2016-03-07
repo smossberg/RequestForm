93223ee91dae230821fc3ffbf716ffc7c60097d1
moss <moss@mossburk.example.com>
2016-03-07 23:03:14 +0100

    har nu en has_many :through. Även fått till en del av routingen så att man kan skapa actors standalone utan association, men även att så att man kan skapa en actor direkt i Requesten, en del pill kvar

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,4 @@
 class Actor < ActiveRecord::Base
-	has_and_belongs_to_many :requests, :class_name => "Request", :join_table => :actors_requests
+	has_many :request_actors
+	has_many :requests, through: :request_actors
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,7 @@
 class Request < ActiveRecord::Base
 	has_one :business_context, :class_name => "BusinessContext"
-	has_and_belongs_to_many :actors,  :class_name => "Actor", :join_table => "actors_requests"
 	validates :name, presence: true, length: { minimum: 5 }
+
+	has_many :request_actors
+	has_many :actors, through: :request_actors
 end
