d28412a059e2522bbfd83731b78ac1926d7d4f96
moss <moss@ip144-224.mphy.lu.se>
2016-03-08 16:14:00 +0100

    Nu får flow sin provider från flow_actor också

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -1,7 +1,8 @@
 class Flow < ActiveRecord::Base
 	has_many :consumers, :through => :flow_actors, :source => :actor
-	has_one :provider, :through => :flow_actors, :source => :actor
+	has_one :provider, :through => :flow_actor, :source => :actor
 
 	has_many :flow_actors
+	has_one :flow_actor
 	
 end
