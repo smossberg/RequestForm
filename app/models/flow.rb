a0d06de2ca0d059b5c5ae64c60fe8a29d7feb0b7
moss <moss@ip144-224.mphy.lu.se>
2016-03-09 20:48:51 +0100

    Måste tänka om trots allt. Behöver skapa två tabeller för de olika staten, consumer och provider

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -1,11 +1,9 @@
 class Flow < ActiveRecord::Base
 	has_many :consumers, :through => :flow_actors, :source => :actor
-	has_one :provider, :through => :flow_actor, :source => :actor
+	has_many :providers, :through => :flow_actors, :source => :actor
 
 	has_many :flow_actors
-	has_one :flow_actor
 
 	belongs_to :request
 	
-	accepts_nested_attributes_for :provider
 end
